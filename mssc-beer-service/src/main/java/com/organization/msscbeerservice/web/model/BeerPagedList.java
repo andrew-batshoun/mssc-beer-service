9f95b26d7a231aac0d83479345c531cfe25785f5
Andrawes Batshoun <andrawes.batshoun@gmail.com>
2023-02-21 11:17:08 -0600

    adding web data model

---
+++ mssc-beer-service/src/main/java/com/organization/msscbeerservice/web/model/BeerPagedList.java
@@ -0,0 +1,20 @@
+package com.organization.msscbeerservice.web.model;
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+/**
+ * @Project mssc-beer-service
+ * @Author andrawesbatshoun on 2/21/23.
+ */
+public class BeerPagedList extends PageImpl<BeerDto> {
+    public BeerPagedList(List<BeerDto> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public BeerPagedList(List<BeerDto> content) {
+        super(content);
+    }
+}
---
+++ mssc-beer-service/src/main/java/com/organization/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,9 @@
+package com.organization.msscbeerservice.web.model;
+
+/**
+ * @Project mssc-beer-service
+ * @Author andrawesbatshoun on 2/21/23.
+ */
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
